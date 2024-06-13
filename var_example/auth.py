2d363d06b8fb0cbcd878d8dc0276926e430d13e9
imageboardsnet <me@imageboards.net>
2024-06-13 16:40:44 +0200

    update example

--- var_example/auth.py
+++ var_example/auth.py
@@ -1,1 +1,3 @@
-secret_key = "secret_key" #hcaptcha secret key
+secret_key = "blossom_secret_key" # secret key for the site
+hcaptcha_sitekey = "" #hcaptcha site key
+hcaptcha_secret_key = "" #hcaptcha secret key
