013cec135c625985c06a0e281c6e7ddfff7587b9
imageboardsnet <me@imageboards.net>
2024-06-16 21:41:27 +0200

    let's be safe

--- app.py
+++ app.py
@@ -297,14 +297,14 @@ def imageboards_json():
 def imageboards_legacy_json():
     return get_endpoints(legacy=True)
 
-@app.route('/initib')
-@login_required
-def initib():
-    if current_user.role != "admin":
-        return redirect(url_for('dashboard'))
-    imageboardsl = imageboardsb()
-    imageboardsl.assign_fields()
-    return redirect(url_for('dashboard'))
+# @app.route('/initib')
+# @login_required
+# def initib():
+#     if current_user.role != "admin":
+#         return redirect(url_for('dashboard'))
+#     imageboardsl = imageboardsb()
+#     imageboardsl.assign_fields()
+#     return redirect(url_for('dashboard'))
 
 @app.route('/about')
 def about():
