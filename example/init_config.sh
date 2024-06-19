0f43cff6ca323bf75897d39cb53a041be578c2ca
imageboardsnet <me@imageboards.net>
2024-06-19 14:25:29 +0200

    move init script

---
+++ example/init_config.sh
@@ -0,0 +1,3 @@
+cp -r example/data ../data
+cp -r example/endpoints ../endpoints
+cp -r example/var ../var
--- init.sh
+++
@@ -1,3 +0,0 @@
-cp -r example/data data
-cp -r example/endpoints endpoints
-cp -r example/var var
